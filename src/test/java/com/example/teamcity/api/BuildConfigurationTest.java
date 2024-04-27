c392889ebbf0de3b06a8e64278f124ef534e89c1
a1ya <alevtina.gatilova@gmail.com>
2024-04-27 23:54:04 +0200

    [QAA] test 1st commit

---
+++ src/test/java/com/example/teamcity/api/BuildConfigurationTest.java
@@ -0,0 +1,25 @@
+package com.example.teamcity.api;
+
+import com.example.teamcity.api.generators.TestDataStorage;
+import com.example.teamcity.api.requests.checked.CheckedUser;
+import org.testng.annotations.Test;
+import com.example.teamcity.api.requests.checked.CheckedProject;
+import com.example.teamcity.api.spec.Specifications;
+
+public class BuildConfigurationTest extends BaseApiTest{
+    @Test
+    public void buildConfigurationTest(){
+        var testData = TestDataStorage.getStorage().addTestData();
+
+        new CheckedUser(Specifications.getSpec().superUserSpec()).create(testData.getUser());
+
+        var project = new CheckedProject(Specifications.getSpec()
+                .authSpec(testData.getUser()))
+                .create(testData.getProject());
+
+        softy.assertThat(project.getId()).isEqualTo(testData.getProject().getId());
+    }
+
+    //project1 - "name": "test_nacVquQTcI"
+    //project2 - "name": "test_nacVquQTcI"
+}
