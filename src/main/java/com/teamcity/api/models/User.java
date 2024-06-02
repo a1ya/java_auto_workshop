b74ef90a5086cd4a2c70883577339d9ba15f0d33
a1ya <alevtina.gatilova@gmail.com>
2024-06-02 14:19:24 +0200

    [QAA] edits

--- src/main/java/com/teamcity/api/models/User.java
+++ src/main/java/com/teamcity/api/models/User.java
@@ -1,6 +1,5 @@
 package com.teamcity.api.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,7 +9,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
     private String username;
     private String password;
